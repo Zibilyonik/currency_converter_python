e57c53f5e002d0a82115dc18766f3b5e6a761db8
Onur Surmeli <onatsurmeli@gmail.com>
2023-04-27 23:42:46 +0200

    adjusted code for personal use

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def load_data(url):
 def main():
     currency = input()
     currency_val = load_data(f'http://www.floatrates.com/daily/{currency}.json')
-    print(currency_val['usd'])
-    print(currency_val['eur'])
+    print(f'USD: {round(currency_val["usd"]["rate"], 2)}, inverse: {round(currency_val["usd"]["inverseRate"], 2)}')
+    print(f'EUR: {round(currency_val["eur"]["rate"], 2)}, inverse: {round(currency_val["eur"]["inverseRate"], 2)}')
+    print(f'TRY: {round(currency_val["try"]["rate"], 2)}, inverse: {round(currency_val["try"]["inverseRate"], 2)}')
 
 
 if __name__ == '__main__':
