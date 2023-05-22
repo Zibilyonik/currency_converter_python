6578114ef841eecec371e5056500000ba872ec64
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-05-22 16:13:43 +0200

    completed currency converter project

--- main.py
+++ main.py
@@ -8,32 +8,36 @@
         main() - main function to grab currency values and print them
 
 '''
-import json
-import requests
-
-
 def load_data(url):
-    '''grabs data from the provided url and returns the text'''
-    response = requests.get(url, timeout="5")
+    response = requests.get(url)
     return json.loads(response.text)
 
 
 def main():
-    '''main function to grab currency values and print them'''
-    currency = input().lower()
+    rates = {}
+    currency = input()
     if currency == "":
-        currency = 'pln'
+        return
+    rates[currency] = 1
     currency_val = load_data(f'http://www.floatrates.com/daily/{currency}.json')
-    print(
-        f'USD: {round(currency_val["usd"]["rate"], 2)}, \
-            inverse: {round(currency_val["usd"]["inverseRate"], 2)}')
-    print(
-        f'EUR: {round(currency_val["eur"]["rate"], 2)}, \
-            inverse: {round(currency_val["eur"]["inverseRate"], 2)}')
-    print(
-        f'TRY: {round(currency_val["try"]["rate"], 2)}, \
-            inverse: {round(currency_val["try"]["inverseRate"], 2)}')
+    if currency != 'eur':
+        rates['eur'] = currency_val['eur']['rate']
+    if currency != 'usd':
+        rates['usd'] = currency_val['usd']['rate']
 
+    while True:
+        target = input()
+        if target == "":
+            break
+        amount = input()
+        if amount == "":
+            break
+        print('Checking the cache...')
+        if target in rates:
+            print('Oh! It is in the cache!')
+        else:
+            print('Sorry, but it is not in the cache!')
+            rates[target] = currency_val[target]['rate']
+        print(f'You received {round(float(amount) * float(rates[target]), 2)} {target.upper()}.')
 
-if __name__ == '__main__':
-    main()
+main()
